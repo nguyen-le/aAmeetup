47cef0abb6483424898d08869098c4ef6ffe32b0
Nguyen Le <nguyentle7@gmail.com>
2014-09-12 22:58:41 -0700

    can now vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,18 @@
 class VotesController < ApplicationController
+  def create
+    @feature = Feature.find(params[:feature_id])
+    @vote    = @feature.votes.new(user_id: current_user.id)
+
+    if @vote.save
+      flash[:notice] = 'Vote was successfully created.'
+      redirect_to home_url
+    else
+      redirect_to "/home"
+    end
+  end
+
+  private
+
+  def vote_params
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   end
   resources :chats
   resources :users
-  resources :features
+  resources :features do
+    resources :votes, only: [:create]
+  end
   resource  :session, only: [:new, :create, :destroy]
 end
